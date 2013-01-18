7524cf49ac40768384c45568c9c8824b1bf129d7
wballestero <willmejia1011@hotmail.com>
2013-01-17 20:38:14 -0600

    Update default.aspx.cs

--- default.aspx.cs
+++ default.aspx.cs
@@ -17,20 +17,16 @@ namespace TEST
         public static class Alert
         {
 
-            /// <summary>
-            /// Shows a client-side JavaScript alert in the browser.
-            /// </summary>
-            /// <param name="message">The message to appear in the alert.</param>
             public static void Show(string message)
             {
-                // Cleans the message to allow single quotation marks
+ 
                 string cleanMessage = message.Replace("'", "\\'");
                 string script = "<script type=\"text/javascript\">alert('" + cleanMessage + "');</script>";
 
-                // Gets the executing web page
+
                 Page page = HttpContext.Current.CurrentHandler as Page;
 
-                // Checks if the handler is a Page and that the script isn't allready on the Page
+
                 if (page != null && !page.ClientScript.IsClientScriptBlockRegistered("alert"))
                 {
                     page.ClientScript.RegisterClientScriptBlock(typeof(Alert), "alert", script);
@@ -44,10 +40,6 @@ namespace TEST
             string cadena = "Data Source=COLOSSUS\\SQLEXPRESS;Initial Catalog=tito;Integrated Security=True";
             SqlConnection conexion = new SqlConnection(cadena);
 
-            //   conexion.Open();
-            //  SqlDataAdapter adp = new SqlDataAdapter(" select * from clientes ", conexion);
-            // DataGrid datos = new DataGrid();
-            // adp.Fill(datos, "clientes");
 
             try
             {
